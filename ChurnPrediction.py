80cf3d8b7b19292e465c4ab80924a1466549a6b2
Ayush Raj <rajayush213@gmail.com>
2021-02-01 00:12:28 +0530

    Initialized and compiled the ANN

--- ChurnPrediction.py
+++ ChurnPrediction.py
@@ -62,6 +62,25 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
+#initializing ANN with Sequential()
+classifier = Sequential()
+# Adding Input Layer And first Hidden layer to ANN
+classifier.add(keras.Input(shape=(11,)))
+classifier.add(Dense(units= 6, activation= "relu", kernel_initializer='uniform')) 
+#kernel_initializer='uniform' initializes weights uniformly near zero
+# units are the number of nodes in the layer
+
+#Adding Second hidden layer
+classifier.add(Dense(units = 6, activation='relu', kernel_initializer='uniform'))
+
+#Adding Output layer 
+classifier.add(Dense(units=1, activation = 'sigmoid', kernel_initializer='uniform'))
+# Output layer will have only one node, thus units = 1
+# Output is the binary prediction so used sigmoid function as activation function
+
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
 
 
 
